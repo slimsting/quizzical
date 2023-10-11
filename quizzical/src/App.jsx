f82bb027834418d90bdf475576b62bbef5ebb1e1
slimsting <newbon.gadson@gmail.com>
2023-10-11 19:12:51 +0300

    this is the most stable version so far need to check why the startSection component does not lead when props are validated

---
+++ quizzical/src/App.jsx
@@ -0,0 +1,22 @@
+import "./App.css";
+import Start from "./components/Start";
+import Questions from "./components/Questions";
+import React from "react";
+
+function App() {
+  const [isQuestions, setIsQuestions] = React.useState(false);
+  function startQuiz() {
+    setIsQuestions((oldIsQuestions) => !oldIsQuestions);
+    console.log("Quiz started");
+  }
+  return (
+    <main>
+      <div id="blob-1" className="blob"></div>
+      <div id="blob-2" className="blob"></div>
+
+      {isQuestions ? <Questions /> : <Start startQuiz={startQuiz} />}
+    </main>
+  );
+}
+
+export default App;
